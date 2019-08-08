d0baf73a433d35b397992ce52c89071f4f327836
dillonlaughter <39819735+dillonlaughter@users.noreply.github.com>
2019-08-07 22:44:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -390,7 +390,7 @@ document.querySelector("#ctl00_MainPlaceHolder_radtbDate_dateInput_text").stlye 
 
 //Auto-format Phone number
 if (document.querySelector("#ctl00_MainPlaceHolder_radgrdSearchRetailCustomers_ctl00 > tbody") != null) {
-    document.querySelector("#ctl00_MainPlaceHolder_radgrdSearchRetailCustomers_ctl00__0 > td:nth-child(4)").style.width='7%';
+    document.querySelector("#ctl00_MainPlaceHolder_radgrdSearchRetailCustomers_ctl00__0 > td:nth-child(4)").style.width='8%';
     for (i=0;i<document.querySelector("#ctl00_MainPlaceHolder_radgrdSearchRetailCustomers_ctl00 > tbody").rows.length;i++){
         if (document.querySelector("#ctl00_MainPlaceHolder_radgrdSearchRetailCustomers_ctl00 > tbody").rows[i].cells[3].innerText != " "){
             splicetemp = document.querySelector("#ctl00_MainPlaceHolder_radgrdSearchRetailCustomers_ctl00 > tbody").rows[i].cells[3].innerText.split('');
